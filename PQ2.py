27ebf472280042d193034d817ce2f7bec59ca329
Samaksh-Manhas <samakshmanhas@gmail.com>
2025-09-24 10:51:14 +0530

    Update PQ2.py

--- PQ2.py
+++ PQ2.py
@@ -4,9 +4,8 @@ monthly_income=float(input("Enter your monthly income: "))
 existing_loan=float(input("Enter your Existing loan: "))
 cibil_score=int(input("Enter your CIBIL Score: "))
 
-if 21>=age<=60:
+if not(21<=age<=60):
     print("Rejected due to age not between 21-60")
-
 elif monthly_income < 25000:
     print("Rejected due to low monthly income")
 elif existing_loan > (0.5 * monthly_income):
